918c3fddd4da120e87d10cd3f99d54957e0859c8
opal-Pachara <s6404062610324@email.kmutnb.ac.th>
2025-05-03 14:29:11 +0700

    change code

--- code/main.py
+++ code/main.py
@@ -1,7 +1,25 @@
 import streamlit as st
+from code import data_model  # แก้การ import ให้ถูกต้อง
 
-st.title("Hello world!")
+# ------------------------------
+# Sidebar menu
+# ------------------------------
+st.sidebar.title("Menu")
+page = st.sidebar.radio("Go to", ["Home", "Data"])
 
-st.markdown("Dashborad Source code !!!")
+# ------------------------------
+# Page rendering
+# ------------------------------
+def main():
+    if page == "Home":
+        st.title("Hello world!")
+        st.markdown("Dashboard Source Code !!!")
 
-st.markdown
+    elif page == "Data":
+        data_model()
+
+# ------------------------------
+# Run the app
+# ------------------------------
+if __name__ == "__main__":
+    main()
